16321b2dff0e6c89141b72799b7dce9cbc56317f
admin <admin@DESKTOP-GRJQEQ2>
2023-10-26 15:01:49 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const callBackendAPI = async () => {
+    const response = await fetch('/express_backend');
+    const body = await response.json();
+
+    if (response.status !== 200) {
+      throw Error(body.message)
+    }
+    return body;
+  };
+
+  const [state, setState] = useState(null);
+
+  // получение GET маршрута с сервера Express, который соответствует GET из server.js
+  useEffect(() => {
+    callBackendAPI()
+        .then(res => setState(res.express))
+        .catch(err => console.log(err));
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+
+        {/* вывод данных, полученных с сервера Express */}
+        <div>
+
+          {state}
+        </div>
+      </div>
   );
 }
 
