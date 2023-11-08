d10f5f8187013ad0a06da480629c482ac60ba1ef
dnestrovsky <serj2356@gmail.com>
2023-11-08 20:03:18 +0300

    Some changes with top menu & top arrow

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,4 +1,7 @@
 import React, { useEffect } from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 import kot from './../images/kot.jpg';
 import './../styles/aboutUs.scss';
 
@@ -20,6 +23,19 @@ const AboutUs = () => {
         });
     }, []);
 
+
+
+
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplayspeed: 1500
+    };
+
     return (
         <section className="container mart" id="AboutUs">
             <div className="row">
@@ -28,20 +44,20 @@ const AboutUs = () => {
                     <p className="fs-4 txt_media">Привет, я Света! Косметолог эстетист с медицинским образованием. В индустрии красоты с 2010 года. В своей работе использую не только проверенные временем методики, но и инновационные технологии для поддержания красоты и здоровья вашей кожи.</p>
                 </div>
                 <div className="col-md-6">
-                    <div className="row">
-                        <div className="col-md-6 mb-3 animate-from-top">
+                    <Slider {...settings}>
+                        <div className="mb-3 animate-from-top">
                             <img src={kot} alt="Image1" className="img-fluid" />
                         </div>
-                        <div className="col-md-6 mb-3 animate-from-top">
+                        <div className="mb-3 animate-from-top">
                             <img src={kot} alt="Image2" className="img-fluid" />
                         </div>
-                        <div className="col-md-6 mb-3 animate-from-top">
+                        <div className="mb-3 animate-from-top">
                             <img src={kot} alt="Image3" className="img-fluid" />
                         </div>
-                        <div className="col-md-6 mb-3 animate-from-top">
+                        <div className="mb-3 animate-from-top">
                             <img src={kot} alt="Image4" className="img-fluid" />
                         </div>
-                    </div>
+                    </Slider>
                 </div>
             </div>
         </section>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -61,8 +61,11 @@ const Header = () => {
                         <h1 className="top__title display-1 text-align-center">Art-Beauty Satyukova</h1>
                         <p className="top__text display-3">Красота как искусство.</p>
                         <a href="" className="top__btn fs-3">Онлайн запись</a>
+
+                        <div className="arrow bounce">
+                            <a href="#AboutUs"><svg xmlns="http://www.w3.org/2000/svg" height="5em" viewBox="0 0 512 512"><path d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z"/></svg></a>
+                        </div>
                     </div>
-                    <a href="#AboutUs"  className="arrows"></a>
                 </div>
             </section>
         </>
